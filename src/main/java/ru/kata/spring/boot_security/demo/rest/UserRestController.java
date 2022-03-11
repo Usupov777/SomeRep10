d3d45b06afdc10cdeaf7810151eb8a74b2e969cb
Egor <usupov777@mail.ru>
2022-03-11 21:50:18 +0700

    Try

--- src/main/java/ru/kata/spring/boot_security/demo/rest/UserRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/rest/UserRestController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/api/admin/")
+@RequestMapping("/api/admin")
 public class UserRestController {
     @Autowired
     private UserService userService;
@@ -30,6 +30,12 @@ public class UserRestController {
     public ResponseEntity<List<User>> getUsers() {
         List<User> users = userService.allUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("/getOne/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Integer id) {
+        User user = userService.getById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @PostMapping("/")
