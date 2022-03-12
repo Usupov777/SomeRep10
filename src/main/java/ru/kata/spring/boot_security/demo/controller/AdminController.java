952296483dfb8d60a313fc842a288c62fac06a38
Usupov777 <Usupov777@mail.ru>
2022-03-12 20:15:12 +0700

    Try

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,19 +1,14 @@
 package ru.kata.spring.boot_security.demo.controller;
 
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
-
-import javax.validation.Valid;
 import java.security.Principal;
-import java.util.List;
-import java.util.Set;
+
 
 @Controller
 @RequestMapping("/admin/")
